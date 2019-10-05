b85d36cef04347824159715ac84cec21f3cd57a9
ibrahim Abdullahi <player.ibrahim00@gmail.com>
2019-10-05 16:19:15 +0200

    Update settings.py

--- booksapp/settings.py
+++ booksapp/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
 
 
 AUTHENTICATION_BACKENDS = [
-        'social_core.backends.linkedin.LinkedinOAuth2',
-        'social_core.backends.instagram.InstagramOAuth2',
+        'social_core.backends.github.GithubOAuth2',
+        'social_core.backends.twitter.TwitterOAuth',
 
         'django.contrib.auth.backends.ModelBackend',
     ]
